6e7f55177e20df118705b0cf517e88f8e71fc1ba
Md Adil Raza <adilsiddiqei30@gmail.com>
2026-02-19 08:41:20 +0530

    String

---
+++ Basic Python (AI-powered) 03/7_F-Strings.py
@@ -0,0 +1,6 @@
+name = input("whats your name: ")
+work = input("where do you work: ")
+live = input("where do you live: ")
+
+# Adil Raza works at Google and lives in New York.
+print(f"{name} works at {work} and lives in {live}.") # using f-string
